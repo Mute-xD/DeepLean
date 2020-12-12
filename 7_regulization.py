39ee97e01599ed83ed022f2280589c2cbb0cac4d
Mute <tom1272008439@hotmail.com>
2020-12-12 09:50:15 +0800

    update

---
+++ 7_regulization.py
@@ -0,0 +1,33 @@
+import keras as k
+
+
+class MNIST:
+    def __init__(self):
+        self.model = k.models.Sequential()
+        self.opt = k.optimizers.SGD(learning_rate=0.2)
+        self.reg = k.regularizers.l2(.001)
+        (self.xTrain, self.yTrain), (self.xTest, self.yTest) = k.datasets.mnist.load_data()
+
+    def preProcess(self):
+        self.xTrain = self.xTrain.reshape(self.xTrain.shape[0], -1) / 255.0
+        self.xTest = self.xTest.reshape(self.xTest.shape[0], -1) / 255.0
+
+        self.yTrain = k.utils.to_categorical(self.yTrain, )
+        self.yTest = k.utils.to_categorical(self.yTest)
+
+    def build(self):
+        self.model.add(k.layers.Dense(200, input_dim=784, activation='tanh', kernel_regularizer=self.reg))
+        self.model.add(k.layers.Dense(100, 'tanh', kernel_regularizer=self.reg))
+        self.model.add(k.layers.Dense(10, 'softmax', kernel_regularizer=self.reg))
+        self.model.compile(optimizer=self.opt, loss='categorical_crossentropy', metrics=['accuracy'])
+        self.model.fit(self.xTrain, self.yTrain, batch_size=32, epochs=20)
+        loss, acc = self.model.evaluate(self.xTest, self.yTest)
+        print('\nTest Set:\n', 'Loss: ', loss, '  Acc: ', acc)
+
+
+if __name__ == '__main__':
+    mnist = MNIST()
+    print('\nxTrain: ', mnist.xTrain.shape, '\nyTrain: ', mnist.yTrain.shape,
+          '\nxTest: ', mnist.xTest.shape, '\nyTest: ', mnist.yTest.shape)
+    mnist.preProcess()
+    mnist.build()
